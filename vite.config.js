68ff1bc45613f3fd0074530cf25a9cf308740f11
lmontes18 <lesy_montesz40@unicah.edu>
2026-03-07 13:48:51 -0600

    Primer commit invitacion XV

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import * as glob from "glob";
+import path, { resolve } from "node:path";
+/* import htmlPurge from 'vite-plugin-purgecss';
+import handlebars from 'vite-plugin-handlebars'; */
+/* import {generarContextoDePagina} from './data/index';  */
+
+const getHtmlEntries = ()=>{
+    return Object.fromEntries(
+        [
+            ...glob.sync('./**/*.html', { ignore:['./dist/**','./node_modules/**']}).map(file=>[
+                file.slice(0, file.length - path.extname(file).length),
+                resolve(__dirname, file)
+            ])
+        ]
+    )
+}
+export default defineConfig(
+    {
+        appType: 'mpa',
+        base: "/",
+        build: {
+            rollupOptions: {
+                input: getHtmlEntries()
+            }
+        },
+       /*  server: {
+    host: true, // Esto permite que se use la IP de tu red local
+    port: 5173,
+  }, */
+        
+    }
+    
+);
